57569fa209a6c02d54ae9c25b729362f3c6cefb6
Rafael de Alvarenga Reis <rafaareis.ebac@gmail.com>
2023-06-06 15:29:08 -0300

    Commit 06062023-1528: Parte Final

--- card.js
+++ card.js
@@ -11,7 +11,9 @@ const cardObj = [
         spotifyLink:'https://open.spotify.com/intl-pt/artist/4MzJMcHQBl9SIYSjwWn8QW',
         spotifyMensalListeners: '1.723.982',
         instagramLink: 'https://www.instagram.com/spiritboxmusic/',
-        instagramFollowers: '278.767'
+        instagramFollowers: '278.767',
+        about: "Spiritbox é uma banda canadense de heavy metal de Victoria, British Columbia. O guitarrista Mike Stringer, marido e mulher, e a vocalista Courtney LaPlante estabeleceram publicamente o Spiritbox em 9 de outubro de 2017.",
+        wikipediaLink: "https://en.wikipedia.org/wiki/Spiritbox"
     },
     {
         id: 2,
@@ -20,12 +22,14 @@ const cardObj = [
         country: 'country/united-kingdom.png',
         genre: 'Alternative',
         img1: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPKdK3IsvYF_gWjdTs0EvAbBs8qT3F07aeMw&usqp=CAU',
-        img2: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkqLpqNh8FLwgdnIOytoqJC2HjftGi1fQu_w&usqp=CAU',
-        img3: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6Q8NL5DiLniTJNbyeeOfD1tws3LxNnJi_cA&usqp=CAU',
+        img2: "https://www.allthingsloud.com/wp-content/uploads/2017/11/RoyalBloodAFAS.jpg",
+        img3: 'https://purepop.com.br/wp-content/uploads/2022/03/Royal-Blood-1200x900.jpg',
         spotifyLink:'https://open.spotify.com/intl-pt/artist/2S5hlvw4CMtMGswFtfdK15',
         spotifyMensalListeners: '3.736.356',
         instagramLink: 'https://www.instagram.com/royalblooduk/',
-        instagramFollowers: '431.893'
+        instagramFollowers: '431.893',
+        about: "Royal Blood é um duo de rock britânico formada em Brighton em 2013. O som da banda é uma reminiscência de garage rock e blues rock.",
+        wikipediaLink: "https://pt.wikipedia.org/wiki/Royal_Blood_(banda)"
     },
     {
         id: 3,
@@ -39,7 +43,9 @@ const cardObj = [
         spotifyLink:'https://open.spotify.com/intl-pt/artist/7o6cOczXTB8ioTAAJTbESf',
         spotifyMensalListeners: '534.833',
         instagramLink: 'https://www.instagram.com/jinjer_official/',
-        instagramFollowers: '500.575'
+        instagramFollowers: '500.575',
+        about: "Jinjer é uma banda ucraniana de metal progressivo formada em 2009. O seu estilo músical incorpora elementos do death metal, metalcore e nu-metal.",
+        wikipediaLink: "https://pt.wikipedia.org/wiki/Jinjer"
     },
     {
         id: 4,
@@ -53,7 +59,9 @@ const cardObj = [
         spotifyLink:'https://open.spotify.com/intl-pt/artist/5INjqkS1o8h1imAzPqGZBb',
         spotifyMensalListeners: '25.082.553',
         instagramLink: 'https://www.instagram.com/tameimpala/',
-        instagramFollowers: '2.832.308'
+        instagramFollowers: '2.832.308',
+        about: "Tame Impala é um grupo de música psicodélica australiano fundado pelo multi-instrumentista Kevin Parker em 2007. Em estúdio, Parker compõe, grava, executa e produz sozinho as músicas.",
+        wikipediaLink: "https://pt.wikipedia.org/wiki/Tame_Impala"
     },
     {
         id: 5,
@@ -67,7 +75,9 @@ const cardObj = [
         spotifyLink:'https://open.spotify.com/intl-pt/artist/5INjqkS1o8h1imAzPqGZBb',
         spotifyMensalListeners: '479.304',
         instagramLink: 'https://www.instagram.com/larkinpoe/',
-        instagramFollowers: '269.326'
+        instagramFollowers: '269.326',
+        about: "Larkin Poe é uma dupla de blues estadunidense originária da Georgia, formada pelas irmãs Rebeca Lovell e Megan Lovell. A dupla combina blues tradicional com guitarra slide. Elas são bastante conhecidas como as irmãs mais novas dos Allman Brothers.",
+        wikipediaLink: "https://pt.wikipedia.org/wiki/Larkin_Poe"
     },
     {
         id: 6,
@@ -81,7 +91,9 @@ const cardObj = [
         spotifyLink:'https://open.spotify.com/intl-pt/artist/2lmQ4CUnjmLIIfnwZdRmMY',
         spotifyMensalListeners: '131.813',
         instagramLink: 'https://www.instagram.com/bluespills/',
-        instagramFollowers: '34.902'
+        instagramFollowers: '34.902',
+        about: "Blues Pills é uma banda de rock sueca, originaria da cidade de Örebro. Formada atualmente pela vocalista Elin Larsson, o guitarrista Zach Anderson, o baixista Kristoffer Schander e o baterista André Kvarnström, Rickard Nygren é um membro convidado da banda, que toca guitarra e órgão em sessões da banda.",
+        wikipediaLink: "https://pt.wikipedia.org/wiki/Blues_Pills"
     },
     {
         id: 7,
@@ -95,7 +107,9 @@ const cardObj = [
         spotifyLink:'https://open.spotify.com/intl-pt/artist/2ye2Wgw4gimLv2eAKyk1NB',
         spotifyMensalListeners: '25.834.491',
         instagramLink: 'https://www.instagram.com/metallica/',
-        instagramFollowers: '9.888.073'
+        instagramFollowers: '9.888.073',
+        about: "Metallica é uma banda norte-americana de heavy metal originária de Los Angeles, mas com base em San Francisco.",
+        wikipediaLink: "https://pt.wikipedia.org/wiki/Metallica"
     },
     {
         id: 8,
@@ -109,7 +123,9 @@ const cardObj = [
         spotifyLink:'https://open.spotify.com/intl-pt/artist/2SmW1lFlBJn4IfBzBZDlSh',
         spotifyMensalListeners: '562.808',
         instagramLink: 'https://www.instagram.com/thewarningrockband/',
-        instagramFollowers: '450.210'
+        instagramFollowers: '450.210',
+        about: "The Warning é uma banda de rock de Monterrey, no México, iniciada em 2013 por três irmãs, Daniela, Paulina e Alejandra Villarreal que ficaram conhecidas pelo cover da música 'Enter Sandman' da banda de heavy metal Metallica, em sua página do YouTube, ganhando atenção de artistas como Muse e Coldplay.",
+        wikipediaLink: "https://pt.wikipedia.org/wiki/The_Warning_(banda)"
     },
     {
         id: 9,
@@ -123,7 +139,9 @@ const cardObj = [
         spotifyLink:'https://open.spotify.com/intl-pt/artist/4pejUc4iciQfgdX6OKulQn',
         spotifyMensalListeners: '7.748.699',
         instagramLink: 'https://www.instagram.com/queensofthestoneage/',
-        instagramFollowers: '697.362'
+        instagramFollowers: '697.362',
+        about: "Queens of the Stone Age é uma banda norte-americana de rock formada em Palm Desert, Califórnia em 1996. É muito conhecida por popularizar o gênero stoner rock que até os anos 2000 não era muito conhecido",
+        wikipediaLink: "https://pt.wikipedia.org/wiki/Queens_of_the_Stone_Age"
     },
     {
         id: 10,
@@ -137,7 +155,9 @@ const cardObj = [
         spotifyLink:'https://open.spotify.com/intl-pt/artist/2SNzxY1OsSCHBLVi77mpPQ',
         spotifyMensalListeners: '906.806',
         instagramLink: 'https://www.instagram.com/joebonamassa/',
-        instagramFollowers: '862.178'
+        instagramFollowers: '862.178',
+        about: "Joe Bonamassa é um cantor, guitarrista e compositor de blues-rock estadunidense. ",
+        wikipediaLink: "https://pt.wikipedia.org/wiki/Joe_Bonamassa"
     },
     {
         id: 11,
@@ -151,7 +171,9 @@ const cardObj = [
         spotifyLink:'https://open.spotify.com/intl-pt/artist/0L8ExT028jH3ddEcZwqJJ5',
         spotifyMensalListeners: '29.905.718',
         instagramLink: 'https://www.instagram.com/chilipeppers/',
-        instagramFollowers: '5.353.071'
+        instagramFollowers: '5.353.071',
+        about: "Red Hot Chili Peppers é uma banda de rock dos Estados Unidos formada em Los Angeles, Califórnia, em 1983. O estilo musical do grupo consiste principalmente no funk rock, bem como elementos de outros gêneros, tais como punk, rock alternativo, rap rock e rock psicodélico.",
+        wikipediaLink: "https://pt.wikipedia.org/wiki/Red_Hot_Chili_Peppers"
     },
     {
         id: 12,
@@ -165,13 +187,15 @@ const cardObj = [
         spotifyLink:'https://open.spotify.com/intl-pt/artist/3KdXhEwbqFHfNfSk7L9E87',
         spotifyMensalListeners: '157.137',
         instagramLink: 'https://www.instagram.com/yourbaroness/',
-        instagramFollowers: '121.352'
+        instagramFollowers: '121.352',
+        about: "Baroness é uma banda de heavy metal americana formada em Savannah, Georgia. Sua música incorpora elementos de sludge, rock progressivo, rock alternativo, entre outros elementos.",
+        wikipediaLink: "https://pt.wikipedia.org/wiki/Baroness"
     },
 ]
 
 let cards = cardObj.map(function(card) {
         return `
-                <div class="col-lg-4 col-md-6 col-sm-8">
+                <div class="col-lg-4 col-md-6 col-sm-8" id="card">
                     <article class="card">
                         <div class="card-header">
                             <header>
@@ -189,54 +213,73 @@ let cards = cardObj.map(function(card) {
                                         </div>
                                     </a>  
                                 </div>
-                            </header>
-                        </div>
-                        <div class="card">
-                            <div id="carousel-exemplo-${card.id}" class="carousel carousel-fade slide" data-bs-ride="carousel">
-                                <div class="carousel-inner">
-                                    <div data-bs-interval="5000" class="carousel-item active">
-                                        <img class="d-block" id="img1" src="${card.img1}" alt="Foto de ${card.artist}" />
-                                        <div class="carousel-caption">
-                                            <div class="title-card-1">
-                                                <h2>${card.artist}</h2>
-                                             <img id="countryIcon" src="${card.country}" alt="Foto de ${card.artist}" />
-                                            </div>   
+                            </header>                   
+                            <div class="card text-bg-dark">
+                                <div id="carousel-exemplo-${card.id}" class="carousel carousel-fade slide" data-bs-ride="carousel">
+                                    <div class="carousel-inner">
+                                        <div data-bs-interval="5000" class="carousel-item active">
+                                            <img class="d-block" id="img1" src="${card.img1}" alt="Foto de ${card.artist}" />
+                                            <div class="carousel-caption">
+                                                <div class="title-card-1">
+                                                    <h2>${card.artist}</h2>
+                                                <img id="countryIcon" src="${card.country}" alt="Foto de ${card.artist}" />
+                                                </div>
+                                                <div class="subtitle-card-1">
+                                                    <h3>${card.period}, ${card.genre}</h3>
+                                                </div>   
+                                            </div>
+                                        </div>
+                                        <div data-bs-interval="5000" class="carousel-item">
+                                            <img class="d-block" id="img2" src="${card.img2}" alt="Foto de ${card.artist}" />
+                                            <div class="carousel-caption">
+                                                <div class="title-card-2">
+                                                    <h2>${card.artist}</h2>
+                                                    <img id="countryIcon" src="${card.country}" alt="Foto de ${card.artist}" />
+                                                </div>
+                                                <div class="subtitle-card-2">
+                                                    <h3>${card.period}, ${card.genre}</h3>
+                                                </div>     
+                                            </div>
+                                        </div>
+                                        <div data-bs-interval="5000" class="carousel-item">
+                                            <img class="d-block" id="img3" src="${card.img3}" alt="Foto de ${card.artist}" />
+                                            <div class="carousel-caption">
+                                                <div class="title-card-3">
+                                                    <h2>${card.artist}</h2>
+                                                    <img id="countryIcon" src="${card.country}" alt="Foto de ${card.artist}" />
+                                                </div>
+                                                <div class="subtitle-card-3">
+                                                    <h3>${card.period}, ${card.genre}</h3>
+                                                </div>     
+                                            </div>
                                         </div>
                                     </div>
-                                    <div data-bs-interval="5000" class="carousel-item">
-                                        <img class="d-block" id="img2" src="${card.img2}" alt="Foto de ${card.artist}" />
-                                        <div class="carousel-caption">
-                                            <div class="title-card-2">
-                                                <h2>${card.artist}</h2>
-                                                <img id="countryIcon" src="${card.country}" alt="Foto de ${card.artist}" />
-                                            </div>   
-                                        </div>
-                                    </div>
-                                    <div data-bs-interval="5000" class="carousel-item">
-                                        <img class="d-block" id="img3" src="${card.img3}" alt="Foto de ${card.artist}" />
-                                        <div class="carousel-caption">
-                                            <div class="title-card-3">
-                                                <h2>${card.artist}</h2>
-                                                <img id="countryIcon" src="${card.country}" alt="Foto de ${card.artist}" />
-                                            </div>   
-                                        </div>
+                                    <button data-bs-target="#carousel-exemplo-${card.id}" data-bs-slide="prev"  class="carousel-control-prev" type="button">
+                                        <span class="carousel-control-prev-icon"></span>
+                                    </button>
+                                    <button data-bs-target="#carousel-exemplo-${card.id}" data-bs-slide="next"  class="carousel-control-next" type="button">
+                                        <span class="carousel-control-next-icon"></span>
+                                    </button>
+                                    <div class="carousel-indicators">
+                                        <button type="button" data-bs-target="#carousel-exemplo-${card.id}" data-bs-slide-to="0" class="active"></button>
+                                        <button type="button" data-bs-target="#carousel-exemplo-${card.id}" data-bs-slide-to="1"></button>
+                                        <button type="button" data-bs-target="#carousel-exemplo-${card.id}" data-bs-slide-to="2"></button>
                                     </div>
                                 </div>
-                                <button data-bs-target="#carousel-exemplo-${card.id}" data-bs-slide="prev"  class="carousel-control-prev" type="button">
-                                    <span class="carousel-control-prev-icon"></span>
-                                </button>
-                                <button data-bs-target="#carousel-exemplo-${card.id}" data-bs-slide="next"  class="carousel-control-next" type="button">
-                                    <span class="carousel-control-next-icon"></span>
-                                </button>
-                                <div class="carousel-indicators">
-                                    <button type="button" data-bs-target="#carousel-exemplo-${card.id}" data-bs-slide-to="0" class="active"></button>
-                                    <button type="button" data-bs-target="#carousel-exemplo-${card.id}" data-bs-slide-to="1"></button>
-                                    <button type="button" data-bs-target="#carousel-exemplo-${card.id}" data-bs-slide-to="2"></button>
+                                <div class="about">
+                                    <h1 class="card-title">Sobre</h1>
+                                        <p class="card-text">${card.about}</p>
+                                    <a href="${card.wikipediaLink}" class="btn btn-primary">Leia Mais</a>
                                 </div>
-                            </div>
+                                <footer class="card-footer">
+                                    <b>Atualizado em <time>31/05/2023 às 16:35:00</time></b>
+                                </footer>
+                            </div>      
                         </div>
                     </article>
+                    
                 </div>
+                
                 `;
 });
 
